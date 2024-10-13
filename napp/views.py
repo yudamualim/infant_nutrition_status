598fd7db19071ba6585825ccf708d07ad2bf3f6a
yudamualim <yudamualim68538@gmail.com>
2024-10-14 03:25:45 +0700

    feat: data_test page

--- napp/views.py
+++ napp/views.py
@@ -8,14 +8,20 @@ def data_test(request):
 
   df = pd.read_csv(csv_path, encoding='latin-1')
   
-  # # Data Standardization
+  # Data Standardization
   scaler = StandardScaler()
   x = df[
     ['Berat_Badan', 'Tinggi_Badan', 'Usia_Saat_Ukur', 'Jenis_Kelamin']
   ]
   x = scaler.fit_transform(x)
+  
+  # Format data with two decimal places
+  formatted_data = [
+    [f'{value:.2f}' for value in row] for row in x
+  ]
 
   context = {
-    'x': x
+    'x': x,
+    'formatted_data': formatted_data
   }
   return render(request, 'data_test.html', context)  

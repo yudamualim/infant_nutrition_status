133846263b744636d373bc8ca67e6012a9e2386b
yudamualim <yudamualim68538@gmail.com>
2024-10-14 03:08:23 +0700

    feat: implement algorithm and display it raw

--- napp/views.py
+++ napp/views.py
@@ -1,7 +1,21 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from django.shortcuts import render
+from django.contrib.staticfiles import finders
 
 def data_test(request):
+  csv_path = finders.find('data/testings.csv')
+
+  df = pd.read_csv(csv_path, encoding='latin-1')
+  
+  # # Data Standardization
+  scaler = StandardScaler()
+  x = df[
+    ['Berat_Badan', 'Tinggi_Badan', 'Usia_Saat_Ukur', 'Jenis_Kelamin']
+  ]
+  x = scaler.fit_transform(x)
+
   context = {
-    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat velit illum odit inventore magnam nihil iure fuga mollitia cumque eum.',
+    'x': x
   }
   return render(request, 'data_test.html', context)  

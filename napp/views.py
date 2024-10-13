68bbb708ab58dbf357b5c28343074d2b2d3dfc06
yudamualim <yudamualim68538@gmail.com>
2024-10-14 02:50:03 +0700

    feat: set static assets for project

--- napp/views.py
+++ napp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def data_test(request):
-  return render(request, '')  
+  context = {
+    'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat velit illum odit inventore magnam nihil iure fuga mollitia cumque eum.',
+  }
+  return render(request, 'data_test.html', context)  

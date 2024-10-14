212cb9d983b0e25f76e25be5abd7876f8e7b7325
yudamualim <yudamualim68538@gmail.com>
2024-10-14 17:11:17 +0700

    feat: define models

--- napp/models.py
+++ napp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+  NIK = models.CharField(max_length=16)
+  Nama = models.CharField(max_length=50)
+  Berat_Badan = models.FloatField()
+  Tinggi_Badan = models.FloatField()
+  Usia_Saat_UKur = models.IntegerField()
+  Jenis_Kelamin = models.CharField(max_length=1)
+  Status_Gizi_Balita = models.CharField(max_length=20)
